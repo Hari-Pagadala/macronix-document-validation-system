e30817c8bdda87b37d5cdf7b3bb85cf9b2e43cfc
Hari Pagadala <haripagadala6819@gmail.com>
2025-12-12 08:24:13 +0530

    vendor management page removed heading

--- frontend/src/pages/VendorDashboard.js
+++ frontend/src/pages/VendorDashboard.js
@@ -94,12 +94,6 @@ const VendorDashboard = () => {
           <Box>
             {/* Statistics Cards */}
             <Grid container spacing={3} sx={{ mb: 4 }}>
-              <Grid item xs={12}>
-                <Typography variant="h5" gutterBottom>
-                  Dashboard Overview
-                </Typography>
-              </Grid>
-              
               <Grid item xs={12} sm={6} md={3}>
                 <Card>
                   <CardContent>
